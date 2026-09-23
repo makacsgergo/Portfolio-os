4487f9db572ba4f98a28e581c83efd9611bd1f4a
makacsgergo <153936116+makacsgergo@users.noreply.github.com>
2026-09-23 17:41:18 +0200

    Refresh PWA for company menus

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='portfolio-os-v5';
+const CACHE='portfolio-os-v6';
 const ASSETS=['./','./index.html','./app.css','./app.js','./universe.json','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
