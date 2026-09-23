1fa8fc56ab880c6b7cda532bfd1e442dfe9d45e9
makacsgergo <153936116+makacsgergo@users.noreply.github.com>
2026-09-23 16:39:27 +0200

    Add Portfolio OS sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='portfolio-os-v2';const ASSETS=['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
